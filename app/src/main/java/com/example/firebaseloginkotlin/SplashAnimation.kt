5358a6e0004660d1080cc7868d6c7959fc48094f
thunder609 <oswaldo-pachco@hotmail.es>
2022-07-05 02:38:51 -0400

    Animacion enlazado con el login

---
+++ app/src/main/java/com/example/firebaseloginkotlin/SplashAnimation.kt
@@ -0,0 +1,56 @@
+package com.example.firebaseloginkotlin
+
+import android.animation.Animator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
+import android.widget.Toast
+import android.view.animation.Animation
+import com.example.firebaseloginkotlin.databinding.ActivitySplashAnimationBinding
+
+class SplashAnimation : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashAnimationBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashAnimationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.spAnimation.playAnimation()
+
+
+
+        binding.spAnimation.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {}
+            override fun onAnimationRepeat(animation: Animator?) {
+
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {}
+
+
+            override fun onAnimationEnd(animation: Animator?) {
+                val intent = Intent(applicationContext, SignUpActivity::class.java)
+                intent.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+
+
+
+
+
+            }
+        })
+
+
+    }
+}
+
+
